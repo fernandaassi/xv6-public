1197b9413fec41594cdd7fe52fcc4cbc136022a3
João Hirasawa <joao@estudante.ufscar.br>
2021-01-10 19:15:29 -0300

    Formatação

--- task3.c
+++ task3.c
@@ -4,20 +4,15 @@
 
 int main(int argc, char *argv[]) {
     char *m = (char *)main;
-
     if(fork() == 0) {
         printf(1,"main filho antes da modificacao: %p\n", *m);
         *m = 0;
         printf(1,"main filho apos modificacao: %p\n", *m);
-
         exit();
     }
-
     wait();
-
     printf(1,"main pai antes da modificacao: %p\n", *m);
     *m = 0;
     printf(1,"main pai apos modificacao: %p\n", *m);
-
     exit();
 }
